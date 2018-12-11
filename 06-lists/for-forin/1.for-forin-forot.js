32fdf26eb4e88102d63b3639330ffbc967bfd9fe
LeandroMelloo <leandromoreno1909@hotmail.com>
2018-12-11 16:21:01 -0200

    Atualização lists-06

--- 06-lists/for-forin/1.for-forin-forot.js
+++ 06-lists/for-forin/1.for-forin-forot.js
@@ -1,1 +1,48 @@
-const service = require('./NodeBr/service')
+const service = require("./service");
+
+async function main() {
+  try {
+    const result = await service.obterPessoas("a");
+
+    const names = [];
+
+    /*console.time("for");
+
+    //--------------------------------> FOR <-------------------------------------
+    for (let i = 0; i <= result.results.length - 1; i++) {
+      const pessoa = result.results[i];
+
+      names.push(pessoa.name);
+    }
+
+    console.timeEnd("for");*/
+
+    //-------------------------------> FOR IN <-----------------------------------
+
+    /*console.time("forIn");
+
+    for (let i in result.results) {
+      const pessoa = result.results[i];
+
+      names.push(pessoa.name);
+    }
+
+    console.timeEnd("forIn");*/
+
+    //-------------------------------> FOR OF <------------------------------------
+
+    console.time("forOf");
+
+    for (pessoa of result.results) {
+        names.push(pessoa.name)
+    }
+
+    console.timeEnd("forOf")
+
+    console.log(`Nomes:`, names);
+  } catch (error) {
+    console.error(`Erro interno:`, error);
+  }
+}
+
+main();
