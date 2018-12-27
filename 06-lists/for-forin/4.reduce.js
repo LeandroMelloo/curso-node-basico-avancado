f907000032156db1e596d1c947358d3318414776
LeandroMelloo <leandromoreno1909@hotmail.com>
2018-12-27 10:24:09 -0200

    4.reduce.js

---
+++ 06-lists/for-forin/4.reduce.js
@@ -0,0 +1,38 @@
+// objetivo do reduce é diminuir um array, em um único objeto.
+const { obterPessoas } = require("./service");
+
+Array.prototype.meuReduce = function (callback, valorInicial) {
+  let valorFinal = typeof valorInicial !== undefined ? valorInicial : this[0]
+  for (let index = 0; index <= this.length -1; index ++) {
+    valorFinal = callback(valorFinal, this[index], this)
+  }
+  return valorFinal
+}
+async function main() {
+  try {
+    const { 
+        results 
+    } = await obterPessoas(`a`);
+
+    const pesos = results.map(item => parseInt(item.height))
+    console.log('pesos:', pesos)
+    // [20.2, 30.3, 40,4]
+    // const total = pesos.reduce((anterior, proximo) => {
+    // return anterior + proximo
+    // }, 0)
+
+    const minhaLista = [
+      ['Leandro', 'Lucas'],
+      ['Sistemas de Informação', 'Direito']
+    ]
+    const total = minhaLista.meuReduce((anterior, proximo) => {
+      return anterior.concat(proximo)
+    }, [])
+    .join(', ')
+    console.log('total:', total)
+
+  } catch (error) {
+    console.log("Erro ao obter pessoas", error);
+  }
+}
+main()
